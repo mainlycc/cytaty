4588b3800cd41774b750259049c2258249f20716
Stanislaw Blicharski <mainly.agn@gmail.com>
2025-02-10 06:04:05 +0100

    route9

--- src/app/api/movies/navigation/[id]/route.ts
+++ src/app/api/movies/navigation/[id]/route.ts
@@ -25,13 +25,9 @@ interface TMDBMovie {
   vote_count: number;
 }
 
-type Props = {
-  params: { id: string }
-}
-
 export async function GET(
   request: Request,
-  props: Props
+  { params }: { params: { id: string } }
 ) {
   try {
     const response = await fetch(
@@ -45,7 +41,7 @@ export async function GET(
     const data = await response.json() as TMDBResponse;
     const movies = data.results;
     
-    const currentIndex = movies.findIndex((movie) => movie.id.toString() === props.params.id);
+    const currentIndex = movies.findIndex((movie) => movie.id.toString() === params.id);
     
     const previousId = currentIndex > 0 ? movies[currentIndex - 1].id.toString() : null;
     const nextId = currentIndex < movies.length - 1 ? movies[currentIndex + 1].id.toString() : null;
