6fe5bd02183a3db96881221923ce91e19ffadaca
Stanislaw Blicharski <mainly.agn@gmail.com>
2025-03-05 12:57:38 +0100

    logowanie

--- src/app/components/login-form.tsx
+++ src/app/components/login-form.tsx
@@ -60,7 +60,7 @@ export function LoginForm({
       const { error } = await supabase.auth.signInWithOAuth({
         provider: 'google',
         options: {
-          redirectTo: `${window.location.origin}/auth/callback`,
+          redirectTo: `https://cytomania.pl/auth/callback`,
         },
       })
 
